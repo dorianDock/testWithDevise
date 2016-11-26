37f45a63c50b9c2f193d7336be2a3a6b85551403
Adrien Siami <adrien.siami@gmail.com>
2016-11-26 15:20:23 +0100

    Add photos page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def events
+  end
+
+  def photos
+    @images = Dir.glob("app/assets/images/photos/*.jpg").map do |path|
+      path.split('/').last
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get '/events', to: 'home#events', as: 'events'
+  get '/photos', to: 'home#photos', as: 'photos'
 end
